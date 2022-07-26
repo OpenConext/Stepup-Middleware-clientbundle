224e496da9b61997cddd035f4706e6ba89bf0474
Michiel Kodde <mkodde@ibuildings.nl>
2022-07-26 10:19:59 +0200

    Support vetting type hint retrieval

---
+++ src/Surfnet/StepupMiddlewareClient/Identity/Service/VettingTypeHintService.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * Copyright 2022 SURFnet bv
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Surfnet\StepupMiddlewareClient\Identity\Service;
+
+use Surfnet\StepupMiddlewareClient\Exception\RuntimeException;
+use Surfnet\StepupMiddlewareClient\Identity\Dto\VettingTypeHint;
+use Surfnet\StepupMiddlewareClient\Service\ApiService;
+
+class VettingTypeHintService
+{
+    /**
+     * @var ApiService
+     */
+    private $apiService;
+
+    public function __construct(ApiService $apiService)
+    {
+        $this->apiService = $apiService;
+    }
+
+    public function getOne(string $institution): VettingTypeHint
+    {
+        $result = $this->apiService->read(sprintf('vetting-type-hint/%s', $institution));
+        if (!$result || empty($result)) {
+            throw new RuntimeException(sprintf('No vetting type hint found for institution %s', $institution));
+        }
+        return VettingTypeHint::from($result);
+    }
+}
---
+++ src/Surfnet/StepupMiddlewareClientBundle/Identity/Service/VettingTypeHintService.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * Copyright 2022 SURFnet bv
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Surfnet\StepupMiddlewareClientBundle\Identity\Service;
+
+use Surfnet\StepupMiddlewareClient\Exception\RuntimeException;
+use Surfnet\StepupMiddlewareClient\Identity\Dto\VettingTypeHint;
+use Surfnet\StepupMiddlewareClient\Identity\Service\VettingTypeHintService as LibraryVettingTypeHintService;
+use Surfnet\StepupMiddlewareClientBundle\Exception\NotFoundException;
+
+class VettingTypeHintService
+{
+    /**
+     * @var LibraryVettingTypeHintService
+     */
+    private $vettingTypeHintService;
+
+    public function __construct(LibraryVettingTypeHintService $recoveryTokenService)
+    {
+        $this->vettingTypeHintService = $recoveryTokenService;
+    }
+
+    public function findOne(string $institution): VettingTypeHint
+    {
+        try {
+            return $this->vettingTypeHintService->getOne($institution);
+        } catch (RuntimeException $e) {
+            throw new NotFoundException(
+                sprintf('Vetting type hint not found for institution %s', $institution),
+                $e->getCode(),
+                $e->getPrevious()
+            );
+        }
+    }
+}
